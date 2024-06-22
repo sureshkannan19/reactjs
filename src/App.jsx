8e3226722b9f96562e401be24816ad39b936f5d5
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-06-22 16:00:06 +0530

    Project copied form Maxmilianas part of Udemy Course

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import CoreConceptsSection from "./components/CoreConcepts/CoreConceptsSection.jsx";
-import Header from "./components/Header/Header.jsx";
-import TabButtonSection from "./components/TabButton/TabButtonSection.jsx";
 
 function App() {
-  console.log("Rendering App");
+  
 
   return (
-    <>
-      <Header />
-      <main>
-        <CoreConceptsSection />
-        <TabButtonSection />
-      </main>
-    </>
-  );
+    <h1>React Tic-Tac-Toe</h1>
+  )
 }
 
-export default App;
+export default App
